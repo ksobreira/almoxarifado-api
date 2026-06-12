e95983d7f288ef4dfcbd3c535805cd5f9b7c3d1b
kdev_sobreira <kauam.morais09@gmail.com>
2026-06-12 17:24:06 -0300

    feat: autenticação JWT com BCrypt

--- src/services/app.js
+++ src/services/app.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import authRoutes from './routes/authRoutes.js'
-import categoryRoutes from './routes/categoryRoutes.js'
-import supplierRoutes from './routes/supplierRoutes.js'
-import productRoutes from './routes/productRoutes.js'
-import stockMovementRoutes from './routes/stockMovementRoutes.js'
-import authenticate from './middlewares/authenticate.js'
+import authRoutes from '../routes/authRoutes.js'
+import categoryRoutes from '../routes/categoryRoutes.js'
+import supplierRoutes from '../routes/supplierRoutes.js'
+import productRoutes from '../routes/productRoutes.js'
+import stockMovementRoutes from '../routes/stockMovementRoutes.js'
+import authenticate from '../middlewares/authenticate.js'
 
 const app = express()
 
