4b3fd4ec9cb4dc80821e0e8463d3e33049124234
kdev_sobreira <kauam.morais09@gmail.com>
2026-06-12 16:45:25 -0300

    feat: autenticação JWT com BCrypt

--- src/services/app.js
+++ src/services/app.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import categoryRoutes from '../routes/categoryRoutes.js'
-import supplierRoutes from '../routes/supplierRoutes.js'
-import productRoutes from '../routes/productRoutes.js'
-import stockMovementRoutes from '../routes/stockMovementRoutes.js'
+import authRoutes from './routes/authRoutes.js'
+import categoryRoutes from './routes/categoryRoutes.js'
+import supplierRoutes from './routes/supplierRoutes.js'
+import productRoutes from './routes/productRoutes.js'
+import stockMovementRoutes from './routes/stockMovementRoutes.js'
+import authenticate from './middlewares/authenticate.js'
 
 const app = express()
 
@@ -11,6 +13,10 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.json({ message: 'API Almoxarifado funcionando!' })
 })
+
+app.use('/auth', authRoutes)
+
+app.use(authenticate)
 
 app.use('/categories', categoryRoutes)
 app.use('/suppliers', supplierRoutes)
